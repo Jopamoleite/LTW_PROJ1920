b449833eca5d68e9cc58a0a201abf400e41e5524
Pastilhas <joao.campos1221@gmail.com>
2019-11-28 14:35:03 +0000

    Beautify

--- Project/index.php
+++ Project/index.php
@@ -2,12 +2,15 @@
 
 <body>
   <header>
-    <h1><a href="index.html">casinhas</a></h1>
-    <h2><a href="index.html">gfhgfhg ytggvg</a></h2>
+    <h1><a href="index.html">HouseHunt</a></h1>
+    <form action="login.php" method="post">
+          <p><label>Username: <input type="text" name="username" id="username"></label></p>
+          <p><label>Password: <input type="password" name="password" id="password"></label></p>
+          <button id="loginbutton" type="submit" value="Login">
+    </form>
   </header>
   <div id="signup">
       <a href="register.html">Register</a>
-      <a href="login.html">Login</a>
   </div>
 </body>
 
