f3cb9a73a69ca01a92ba3ab60a29e60829b89900
Pastilhas <joao.campos1221@gmail.com>
2019-11-28 08:54:51 +0000

    Refactor php

---
+++ Project/index.php
@@ -0,0 +1,14 @@
+<? include_once("templates/header.php") ?>
+
+<body>
+  <header>
+    <h1><a href="index.html">casinhas</a></h1>
+    <h2><a href="index.html">gfhgfhg ytggvg</a></h2>
+  </header>
+  <div id="signup">
+      <a href="register.html">Register</a>
+      <a href="login.html">Login</a>
+  </div>
+</body>
+
+<? include_once("templates/footer.php") ?>
