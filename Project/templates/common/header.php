99039e48e5e97f39ca7e850fbb6670a2b9c42d5d
Márcia Teixeira <marcia309@hotmail.com>
2019-12-03 10:41:31 +0000

    Added initial search bar html and css

--- Project/templates/common/header.php
+++ Project/templates/common/header.php
@@ -7,6 +7,8 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap" rel="stylesheet">
     <link href="css/page_style.css" rel="stylesheet">
     <link href="css/page_layout.css" rel="stylesheet">
+    <link href="css/search_bar_layout.css" rel="stylesheet">
+    <link href="css/search_bar_style.css" rel="stylesheet">
   </head>
   <body>
     <div class="page_header">
@@ -15,7 +17,9 @@
           <h1><a href="searchresults.php">HouseHunt</a></h1>
       </div>
       <div id="user_profile">
-          <a href="">Profile</a> <!--TODO link p/ perfil e obter nome/foto do user>-->
+          <a href="">
+          <?php echo $_SESSION["username"]; ?>
+          </a> <!--TODO link p/ perfil e obter nome/foto do user>-->
           <a href=""><img href="" src="http://cdn.onlinewebfonts.com/svg/img_184513.png" alt="User icon" width="20" height="20"></a>
       </div>
     </div>
--- Project/templates/user/searchbar.php
+++ Project/templates/user/searchbar.php
@@ -1,9 +1,21 @@
 <div class="search_bar">
     <form class="search_form" method="post" action="">
-        <div class="search_input_field">
+        <div class="search_input_field_large">
             <label class="search_label">Going to</label>
             <input class="search_input" type="text" placeholder="Destination">
         </div>
-        <div class="">
+        <div class="search_input_field_small">
+            <label class="search_label">Check-In</label>
+            <input class="search_input" type="date" placeholder="dd/mm/yyyy">
         </div>
+        <div class="search_input_field_small">
+            <label class="search_label">Check-Out</label>
+            <input class="search_input" type="date" placeholder="dd/mm/yyyy">
+        </div>
+        <div class="search_input_field_small">
+            <label class="search_label">Guests</label>
+            <input class="search_input" type="number" value="1" min="1" step="1">
+        </div>
+        <button class="submit_search_button" type="submit">Search</button>
+</form>
 </div>
