4495bc1e98213c1c175f9cb849691f311fb479e8
Pastilhas <joao.campos1221@gmail.com>
2019-12-06 14:33:03 +0000

    nao me lembro

--- Project/database/houses.php
+++ Project/database/houses.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 
     function addHouse($title, $address, $price_day, $capacity, $description, $ownerID){
         global $dbh;
         try {
-            $stmt = $dbh->prepare('INSERT INTO Place(title, address_, price_day, capacity, description_, ownerID) 
+            $stmt = $dbh->prepare('INSERT INTO Place(title, address_, price_day, capacity, description_, ownerID)
                                    VALUES (?, ?, ?, ?, ?, ?)');
 
             $stmt->execute(array($title, $address, $price_day, $capacity, $description, $ownerID));
             return "";
-        } catch (PDOException $e) { 
+        } catch (PDOException $e) {
             return "House with that address already registered";
         }
     }
--- Project/main_page.php
+++ Project/main_page.php
@@ -1,7 +1,28 @@
+<!-- HEADER -->
 <?include_once('includes/start.php') ?>
 <?include_once('database/db_user.php') ?>
 <?include_once "templates/common/header.php" ?>
 
+<!-- SEARCH -->
 <?include_once "templates/user/searchbar.php" ?>
 
+<!-- CASINHAS -->
+<div id="housinhas" class="flex_row">
+  <?php
+    addHouse('ola', 'adeus', 420, 2, 'olaadeus', 1);
+    echo "new house";
+
+    $query = sqlite_query($dbh, 'SELECT * FROM Place LIMIT 25');
+    while ($entry = sqlite_fetch_array($query, SQLITE_ASSOC)) {
+      echo "<div class='house'>";
+      echo "<img src='images\default_pic.bmp' id='house pic' alt='House pic' width='300' height='300'>";
+      echo "<h1>" . $entry['address_'] . "</h1>";
+      echo "<h2>" . $entry['title'] . "</h2>";
+      echo "<h2>" . $entry['price_day'] ."€ / night</h2>";
+      echo "</div>";
+    }
+  ?>
+</div>
+
+<!-- FOOTER -->
 <?include_once "templates/common/initial_footer.php" ?>
