3384dfd5721a2fa06b937aceb54925a7ac681c80
Joao Leite <jopamoleite@gmail.com>
2019-12-13 17:38:08 +0000

    Added redirect from houses in main page to house page

--- Project/templates/houses/display_houses.php
+++ Project/templates/houses/display_houses.php
@@ -37,7 +37,7 @@
     if(!empty($destination) && !empty($checkin) && !empty($checkout)) $houses = getHousesAtLocation($destination, $guests); /*TO DO: GET HOUSES AVAILABLE FOR DAYS BETWEEN CHECKIN AND CHECKOUT AND LOCATION = DESTINATION;*/
 
     foreach ($houses as $entry) {
-      echo '<a class="house" href="main_page.php">';
+      echo '<a class="house" href="house_page.php?house='.$entry['id'].'">';
         echo '<img src="images/house.jpg" id="house pic" alt="House pic" width="300" height="300">';
         echo '<h1>' . $entry['location'] . '</h1>';
         echo '<h2>' . $entry['title'] . '</h2>';
