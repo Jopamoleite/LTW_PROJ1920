abdc9f936bc7f3e1bef0dfdf3df8dd786d5cc17a
Joao Leite <jopamoleite@gmail.com>
2019-12-15 18:56:50 +0000

    Merge

--- Project/templates/user/register_check.php
+++ Project/templates/user/register_check.php
@@ -1,14 +1,9 @@
 <?php
-<<<<<<< HEAD
   $path = getcwd();
   chdir('../..');
   include_once 'includes/start.php';
   include_once 'database/db_user.php';
   chdir($path);
-=======
-include_once $_SERVER['DOCUMENT_ROOT'].'/includes/start.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/database/db_user.php';
->>>>>>> 0408c29637f23e3abe5a9d5a3ec53a228a046d00
 
   $type = $_GET['type'];
   $value = $_GET['value'];
