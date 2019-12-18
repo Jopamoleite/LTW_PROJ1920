6d2a6144dfdb484ebb643255618bfeec79c3dfd6
Pastilhas <joao.campos1221@gmail.com>
2019-12-18 04:58:43 +0000

    css buttons

--- Project/templates/edit_house.php
+++ Project/templates/edit_house.php
@@ -8,7 +8,8 @@
 
 <div class="house_edit flex-container">
   <form id ="edit_house_img" action="../actions/action_edit_image.php" method="post" enctype="multipart/form-data">
-    <input oninput="upload()" id="house_img_upload" type="file" name="picture">
+    <input oninput="upload()" id="img_upload" type="file" name="picture">
+          <label for="img_upload">Select file</label>
     <input type="hidden" id="houseID" name="houseID" value="<?php echo $house ?>">
   </form>
   <form id="delete_form" action="../actions/action_delete_house.php" method="post">
