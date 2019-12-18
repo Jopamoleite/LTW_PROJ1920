88c944c8c2078aff5a067c59be9edd6a67160de3
Pastilhas <joao.campos1221@gmail.com>
2019-12-18 03:38:00 +0000

    edit house

---
+++ Project/actions/action_delete_house.php
@@ -0,0 +1,19 @@
+<?php
+  chdir('..');
+
+  include_once 'includes/start.php';
+  include_once 'database/houses.php';
+
+  if($_POST)
+    if($_POST['houseID']){
+      $houseID = $_POST['houseID'];
+      $photo = getHousePhotos($houseID);
+      if ($photo != "default_house.jpg") {
+        unlink("images/" . $photo);
+      }
+
+      removeHouse(intval($houseID));
+    }
+
+  header("Location: ../pages/main_page.php");
+?>
